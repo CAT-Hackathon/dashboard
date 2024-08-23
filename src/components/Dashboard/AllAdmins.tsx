50f5bf91e3ae86cf67e6fda2e737fea8154c0d72
EHAB3x <ehabmah6969@gmail.com>
2024-08-23 20:08:44 +0200

    finish all admins components

---
+++ src/components/Dashboard/AllAdmins.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
+import RenderAdmin from "@components/ui/tables/adminTable/RenderAdmin";
+import useGetAllAdmin from "@hooks/useGetAllAdmin";
+import SkeletonDemo from "@components/common/SkeletonDemo";
+import { Link } from "react-router-dom";
+const AllAdmins = () => {
+  const { isLoading: loadingAdmin } = useGetAllAdmin();
+
+  return (
+    <Card x-chunk="dashboard-01-chunk-5">
+      <CardHeader className="flex flex-col justify-between items-center">
+        <CardTitle className="w-fit">Admins</CardTitle>
+        <Link
+          to="/adduser"
+          className="text-lg font-medium bg-slate-700 text-white rounded-lg px-4 py-2"
+        >
+          Add user
+        </Link>
+      </CardHeader>
+      <CardContent className="grid gap-8">
+        {loadingAdmin ? <SkeletonDemo /> : <RenderAdmin />}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AllAdmins;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,19 +1,10 @@
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@components/ui/card";
-import RenderAdmin from "@components/ui/tables/adminTable/RenderAdmin";
-import useGetAllAdmin from "../hooks/useGetAllAdmin";
-import SkeletonDemo from "@components/common/SkeletonDemo";
-import { Link } from "react-router-dom";
+
 import Stats from "@components/Dashboard/Stats";
 import AllUsers from "@components/Dashboard/AllUsers";
+import AllAdmins from "@components/Dashboard/AllAdmins";
 
 const MainLayout = () => {
-  const { isLoading: loadingAdmin } = useGetAllAdmin();
 
   return (
     <div>
@@ -23,20 +14,7 @@ const MainLayout = () => {
 
           <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
             <AllUsers />
-            <Card x-chunk="dashboard-01-chunk-5">
-              <CardHeader className="flex flex-col justify-between items-center">
-                <CardTitle className="w-fit">Admins</CardTitle>
-                <Link
-                  to="/adduser"
-                  className="text-lg font-medium bg-slate-700 text-white rounded-lg px-4 py-2"
-                >
-                  Add user
-                </Link>
-              </CardHeader>
-              <CardContent className="grid gap-8">
-                {loadingAdmin ? <SkeletonDemo /> : <RenderAdmin />}
-              </CardContent>
-            </Card>
+            <AllAdmins />
           </div>
         </main>
       </div>
