b0ce103006609b1cb810c3e60a163897a2d7cca2
Nabil Alex <nabilmohaed46@gmail.com>
2024-08-24 05:16:42 +0300

    add validations

--- src/validations/addUser.ts
+++ src/validations/addUser.ts
@@ -1,13 +1,21 @@
 import { z } from "zod";
+
 const addUser = z.object({
-  name: z.string({ message: "من فضلك ادحل اسم " }).min(2, "من فضلك ادحل اسم "),
-  role: z.string({ message: "من فضلك ادحل اسم " }).min(1, "من فضلك ادحل اسم "),
-  email: z.string({ message: "من فضلك ادحل حالة" }).min(1, "من فضلك ادحل حالة"),
+  name: z
+    .string({ message: "Please enter a name" })
+    .min(2, "Name must be at least 2 characters long"),
+  role: z.string({ message: "Please enter a role" }).min(1, "Role is required"),
+  email: z
+    .string({ message: "Please enter an email" })
+    .email("Invalid email format"),
   phone: z
-    .string({ message: "من فضلك ادحل رقم تليفون" })
-    .min(1, "من فضلك ادحل رقم تليفون"),
+    .string({ message: "Please enter a phone number" })
+    .min(11, "Phone number is required")
+    .max(11, "Phone number is done")
+    .regex(/^\d+$/, "Phone number must contain only digits"),
   password: z
-    .string({ message: "من فضلك ادحل رقم تليفون" })
-    .min(1, "من فضلك ادحل رقم تليفون"),
+    .string({ message: "Please enter a password" })
+    .min(8, "Password must be at least 8 characters long"),
 });
+
 export { addUser };
